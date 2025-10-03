3970861f70cdcde492274013979001e1b96d3f39
Sephanne Flor <sephanneflor9@gmail.com>
2025-10-03 19:57:27 +0800

    Create Game of Thrones - I.cs

---
+++ Game of Thrones - I.cs
@@ -0,0 +1,67 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gameOfThrones' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string gameOfThrones(string s)
+    {
+         int[] freq = new int[26];
+    
+    
+    foreach (char c in s)
+    {
+        freq[c - 'a']++;
+    }
+    
+    
+    int oddCount = 0;
+    foreach (int count in freq)
+    {
+        if (count % 2 != 0)
+        {
+            oddCount++;
+        }
+    }
+    
+   
+    return (oddCount <= 1) ? "YES" : "NO";
+}
+
+    }
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.gameOfThrones(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
